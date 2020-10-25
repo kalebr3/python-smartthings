588e7fb501e80f0feed61e91914142f6a25a8497
Kaleb Redman <kaleb@redman.us>
2020-10-25 18:11:19 -0400

    Added package docstring

--- SmartThings/__init__.py
+++ SmartThings/__init__.py
@@ -1,1 +1,16 @@
+"""
+Python SmartThings
+
+Classes
+-------
+SmartThings.Account
+
+Methods
+-------
+Account.control_device(deviceId: str, capability: str, command: dict, arguments: dict)
+    Sends a specific command to the specified device.
+Account.execute_scene(sceneId: str)
+    Executes the specified scene.
+"""
+
 from .Account import Account
